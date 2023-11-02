64ed34fab58dd70b75c1980307ba62f4a6467db7
Oleg Smirnov <oleg.smirnoff505@yandex.ru>
2023-11-02 20:35:03 +0500

    Crate main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+
+hostName = "localhost"  # Адрес для доступа по сети
+serverPort = 8080  # Порт для доступа по сети
+
+
+class MyServer(BaseHTTPRequestHandler):
+    """
+        Специальный класс, который отвечает за
+        обработку входящих запросов от клиентов
+    """
+
+    def __get_html_content(self):
+        content_file = 'index.html'
+
+        with open(content_file, 'r') as file:
+            content = file.read()
+            return content
+
+    def do_GET(self):
+        """ Метод для обработки входящих GET-запросов """
+        page_content = self.__get_html_content()
+        self.send_response(200)  # Отправка кода ответа
+        self.send_header("Content-type", "text/html")  # Отправка типа данных, который будет передаваться
+        self.end_headers()  # Завершение формирования заголовков ответа
+        self.wfile.write(bytes(page_content, "utf-8"))  # Тело ответа
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        # Корректный способ остановить сервер в консоли через сочетание клавиш Ctrl + C
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
